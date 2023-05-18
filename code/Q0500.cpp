f014eb5bbde89b21cbf1c0786f4943bb337cd078
Flyingdolar <flyingdollar.cheng@gmail.com>
2023-05-18 23:58:01 +0800

    prog[Q0500]: 做出基本 DFS

--- code/Q0500.cpp
+++ code/Q0500.cpp
@@ -1,6 +1,7 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+
+#define VPII vector<pair<int, int>>
 
 using namespace std;
 
@@ -20,14 +21,67 @@ int greatPair(pair<int, int> a, pair<int, int> b) {
     return a.second - b.second;
 }
 
+void DFS(VPII roadList, VPII stack, int *visit, int &time, VPII &finish) {
+    int curPos = stack.back().first;
+
+    visit[curPos] = DISCOVER, time++;
+    for (auto road : roadList) {
+        if (road.first == curPos && visit[road.second] == ORIGIN) {
+            stack.push_back(make_pair(road.second, time));
+            DFS(roadList, stack, visit, time, finish);
+        }
+    }
+    visit[curPos] = FINISH, time++;
+    finish.push_back(stack.back());
+    finish.back().second = time;
+    stack.pop_back();
+    return;
+}
+
+// Do DFS and setup the value of pos by the order of finish time
+void setDFS(VPII &roadList, VPII &posList) {
+    int time = 0;
+    int *visit = new int[posList.size()];
+    VPII stack, finish;
+
+    toZero(visit, posList.size());
+    for (auto pos : posList) {
+        if (visit[pos.first] == ORIGIN) {
+            stack.push_back(make_pair(pos.first, time));
+            DFS(roadList, stack, visit, time, finish);
+        }
+    }
+
+    sort(finish.begin(), finish.end(), greatPair);
+    for (size_t idx = 0; idx < posList.size(); idx++) {
+        posList[idx].first = finish[idx].first;
+        posList[idx].second = finish[idx].second;
+    }
+    delete[] visit;
+    return;
+}
+
 int main() {
-    int placeNum, routeNum, start, end;
-    pair<int, int> *road;
-    vector<pair<int, int>> road, seq;
+    int posNum, roadNum, start, end;
+    // Data format: Road(Start, End), Pos(Number, Value)
+    VPII road, pos;
 
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cin >> placeNum >> routeNum;
+    cin >> posNum >> roadNum;
+
+    for (int idx = 0; idx < roadNum; idx++) {
+        cin >> start >> end;
+        road.push_back(make_pair(start, end));
+    }
+    for (int idx = 0; idx < posNum; idx++) {
+        pos.push_back(make_pair(idx, 0));
+    }
+
+    setDFS(road, pos);
+    for (auto p : pos) {
+        cout << p.first << " " << p.second << endl;
+    }
 
     return 0;
 }
