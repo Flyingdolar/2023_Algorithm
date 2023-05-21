94ac229a9376dd6d5ef3ec4ae5759455f9a07065
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-05-21 17:45:27 +0800

    solve[Q0502]: 使用 Max min tree 求最佳值

--- code/Q0502.cpp
+++ code/Q0502.cpp
@@ -18,54 +18,24 @@ using namespace std;
 #define COST 2
 #define MAXCOST 1000000000
 
+typedef enum _method_ {
+    MIN = 0,
+    MAX = 1
+} meth;
+
 int cityNum, cabNum;  // Amount of city and cable
 VVI cabList;          // Cable list: Record all cable
 
-// Compare Less Cost
-bool cmpCost(VI &cableA, VI &cableB) {
-    return cableA[COST] < cableB[COST];
-}
-
-bool isBridge(VI cable) {
-    int curCity = cable[START];
-    VB visCity(cityNum, false);
-    VI stack;
-    VVI adjMap(cityNum);
-
-    for (auto cab : cabList) {
-        adjMap[cab[START]].push_back(cab[END]);
-        adjMap[cab[END]].push_back(cab[START]);
-    }
-
-    visCity[cable[START]] = true, visCity[cable[END]] = true;
-    stack.push_back(cable[START]);
-    for (auto cab : adjMap[cable[START]]) {
-        if (cab == cable[END]) continue;
-        if (visCity[cab]) continue;
-        stack.push_back(cab);
-        while (stack.size() > 1) {
-            curCity = stack.back();
-            visCity[curCity] = true;
-            for (auto nextCab : adjMap[curCity]) {
-                if (nextCab == cable[END]) return false;
-                if (visCity[nextCab]) continue;
-                stack.push_back(nextCab);
-                break;
-            }
-            if (stack.back() == curCity) stack.pop_back();
-        }
-    }
-    return true;
-}
-
-// Find Minimum Spanning Tree
-int MST() {
-    int sum = 0, visNum = 0;
-    int curCity = 0, minCost = MAXCOST;
+// Minimum or Maximum Spanning Tree
+VI MST(meth M = MIN, int limit = -1) {
+    int visNum = 0;
+    int curCity = 0, edgeCost;
     VI visCity(cityNum, false);
     VI cityCost(cityNum, MAXCOST);
     VVPI adjMap(cityNum);
 
+    if (M) fill(cityCost.begin(), cityCost.end(), -1);
+    if (limit == -1 && M) limit = MAXCOST;
     for (auto cab : cabList) {
         adjMap[cab[START]].push_back({cab[END], cab[COST]});
         adjMap[cab[END]].push_back({cab[START], cab[COST]});
@@ -74,27 +44,31 @@ int MST() {
     cityCost[0] = 0;
     while (visNum < cityNum) {
         visCity[curCity] = true, ++visNum;
-        minCost = MAXCOST;
+        edgeCost = M ? -1 : MAXCOST;
         for (auto cable : adjMap[curCity]) {
             if (visCity[cable.first]) continue;
-            cityCost[cable.first] = min(cityCost[cable.first], cable.second);
+            if (M && cable.second > limit) continue;
+            if (!M && cable.second < limit) continue;
+            cityCost[cable.first] = M ? max(cityCost[cable.first], cable.second)
+                                      : min(cityCost[cable.first], cable.second);
         }
         for (int idx = 0; idx < cityNum; idx++) {
             if (visCity[idx]) continue;
-            if (cityCost[idx] < minCost) {
-                minCost = cityCost[idx];
-                curCity = idx;
-            }
+            if (M && cityCost[idx] < edgeCost) continue;
+            if (!M && cityCost[idx] > edgeCost) continue;
+            edgeCost = cityCost[idx];
+            curCity = idx;
         }
     }
-    for (auto cost : cityCost) sum += cost;
-    return sum;
+    return cityCost;
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int cityA, cityB, cost, sum = 0;
+    int cityA, cityB;
+    int cost, limit, sum = 0;
+    VI costList;
 
     cin >> cityNum >> cabNum;
     cabList.resize(cabNum);
@@ -103,15 +77,13 @@ int main(void) {
         cityA -= 1, cityB -= 1;
         cabList[idx] = {cityA, cityB, cost};
     }
-    // Sort cabList by cost (Ascending)
-    sort(cabList.begin(), cabList.end(), cmpCost);
-    while (cabList.size() > 0) {
-        VI cable = cabList.front();
-        if (isBridge(cable)) break;
-        cabList.erase(cabList.begin());
-    }
-    // Calculate the mininum cost
-    sum = MST();
+
+    // Calculate the maximum cost
+    costList = MST(MAX);
+    limit = min_element(costList.begin() + 1, costList.end())[0];
+    // Calculate the minimum cost
+    costList = MST(MIN, limit);
+    for (auto cost : costList) sum += cost;
     std::cout << sum << endl;
     return 0;
 }
